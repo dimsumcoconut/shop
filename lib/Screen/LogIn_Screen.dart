8f69bd0ca4868664c61665f02cfad7e7b2e755a4
ed.ginaa <113158996+edginajlvni@users.noreply.github.com>
2024-09-19 00:14:36 +0700

    first commit

---
+++ lib/Screen/LogIn_Screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LogIn extends StatelessWidget {
+  const LogIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Text("Welcome",
+          style: TextStyle(
+            fontSize: 30
+          ),),
+          ),
+      ),
+    );
+  }
+}
